4a8b431ee9bfe7a8d3e3107372cee1a308763604
cuijiacheng.cjc <cuijiacheng.cjc@alibaba-inc.com>
2022-05-03 23:23:56 +0800

    add more editor modules

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -71,4 +71,72 @@ function config.nvim_treesitter()
 	end
 end
 
+function config.autotag()
+	require("nvim-ts-autotag").setup({
+		filetypes = {
+			"html",
+			"xml",
+			"javascript",
+			"typescriptreact",
+			"javascriptreact",
+			"vue",
+		},
+	})
+end
+
+function config.matchup()
+    vim.cmd([[let g:matchup_matchparen_offscreen = {'method': 'popup'}]])
+end
+
+function config.nvim_illuminate()
+    vim.g.Illuminate_highlightUnderCursor = 0
+    vim.g.Illuminate_ftblacklist = {
+        "help",
+        "dashboard",
+        "alpha",
+        "packer",
+        "norg",
+        "DoomInfo",
+        "NvimTree",
+        "Outline",
+        "toggleterm",
+    }
+end
+
+function config.nvim_comment()
+    require("nvim_comment").setup({
+        hook = function()
+            require("ts_context_commentstring.internal").update_commentstring()
+        end,
+    })
+end
+
+function config.toggleterm()
+	require("toggleterm").setup({
+		-- size can be a number or function which is passed the current terminal
+		size = function(term)
+			if term.direction == "horizontal" then
+				return 15
+			elseif term.direction == "vertical" then
+				return vim.o.columns * 0.40
+			end
+		end,
+		open_mapping = [[<c-\>]],
+		hide_numbers = true, -- hide the number column in toggleterm buffers
+		shade_filetypes = {},
+		shade_terminals = false,
+		shading_factor = "1", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+		start_in_insert = true,
+		insert_mappings = true, -- whether or not the open mapping applies in insert mode
+		persist_size = true,
+		direction = "horizontal",
+		close_on_exit = true, -- close the terminal window when the process exits
+		shell = vim.o.shell, -- change the default shell
+	})
+end
+
+function nvim_colorizer()
+	require("colorizer").setup()
+end
+
 return config
--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -8,5 +8,86 @@ editor["nvim-treesitter/nvim-treesitter"] = {
 	event = "BufRead",
 	config = conf.nvim_treesitter,
 }
+editor["nvim-treesitter/nvim-treesitter-textobjects"] = { -- syntax-aware text browse
+	opt = true,
+	after = "nvim-treesitter",
+}
+editor["p00f/nvim-ts-rainbow"] = { -- colorful parentheses
+	opt = true,
+	after = "nvim-treesitter",
+	event = "BufRead",
+}
+editor["JoosepAlviste/nvim-ts-context-commentstring"] = { -- syntax-aware commentstring setting
+    opt = true,
+    after = "nvim-treesitter",
+}
+editor["mfussenegger/nvim-ts-hint-textobject"] = {
+	opt = true,
+	after = "nvim-treesitter",
+}
+editor["windwp/nvim-ts-autotag"] = { -- autotag/autorename html tags
+    opt = true,
+    after = "nvim-treesitter",
+    ft = {"html", "xml"},
+    config = conf.autotag,
+}
+
+-- extend % to match syntax objects
+editor["andymass/vim-matchup"] = {
+    opt = true,
+    after = "nvim-treesitter",
+	config = conf.matchup,
+}
+
+-- faster j/k
+editor["rhysd/accelerated-jk"] = {
+    opt = true,
+    event = "BufReadPost",
+}
+
+-- smarter f/F/t/T
+editor["hrsh7th/vim-eft"] = {
+    opt = true,
+    event = "BufReadPost"
+}
+
+-- auto highlight current syntax-object
+editor["RRethy/vim-illuminate"] = {
+    opt = true,
+    event = "BufRead",
+    config = conf.nvim_illuminate,
+}
+
+-- comment
+editor["terrortylor/nvim-comment"] = {
+    opt = false,
+    config = conf.nvim_comment,
+}
+
+-- align text
+editor["junegunn/vim-easy-align"] = {
+    opt = true,
+    cmd = "EasyAlign"
+}
+
+-- manage/toggle persist terminal windows
+editor["akinsho/toggleterm.nvim"] = {
+	opt = true,
+	event = "BufRead",
+	config = conf.toggleterm,
+}
+
+-- better highlighter
+editor["norcalli/nvim-colorizer.lua"] = {
+	opt = true,
+	event = "BufRead",
+	config = conf.nvim_colorizer,
+}
+
+-- more-friendly buffer delete
+editor["famiu/bufdelete.nvim"] = {
+	opt = true,
+	cmd = { "Bdelete", "Bwipeout", "Bdelete!", "Bwipeout!" },
+}
 
 return editor
