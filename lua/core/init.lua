106cbdc88e3253485dcebd4382e722bb5d0ee380
cuijiacheng.cjc <cuijiacheng.cjc@alibaba-inc.com>
2022-05-04 09:56:02 +0800

    improve init.lua

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -11,13 +11,37 @@ local createdir = function()
 	}
 
 	if vim.fn.isdirectory(globals.cache_dir) == 0 then
-		os.execute("mkdir -p " .. global_cache_dir)
-		for _, v in pair(data_dir) do
+		os.execute("mkdir -p " .. globals.cache_dir)
+		for _, v in pairs(data_dir) do
 			if vim.fn.isdirectory(v) == 0 then
 				os.execute("mkdir -p " .. v)
 			end
 		end
 	end
+end
+
+local disable_distribution_plugins = function()
+	vim.g.did_load_filetypes = 1
+	vim.g.did_load_fzf = 1
+	vim.g.did_load_gtags = 1
+	vim.g.did_load_gzip = 1
+	vim.g.did_load_tar = 1
+	vim.g.did_load_tarPlugin = 1
+	vim.g.did_load_zip = 1
+	vim.g.did_load_zipPlugin = 1
+	vim.g.did_load_getscript = 1
+	vim.g.did_load_getscriptPlugin = 1
+	vim.g.did_load_vimball = 1
+	vim.g.did_load_vimballPlugin = 1
+	vim.g.did_load_matchit = 1
+	vim.g.did_load_matchparen = 1
+	vim.g.did_load_2html_plugin = 1
+	vim.g.did_load_logiPat = 1
+	vim.g.did_load_rrhelper = 1
+	vim.g.did_load_netrw = 1
+	vim.g.did_load_netrwPlugin = 1
+	vim.g.did_load_netrwSettings = 1
+	vim.g.did_load_netrwFileHandlers = 1
 end
 
 local leader_map = function()
@@ -41,26 +65,82 @@ local neovide_config = function()
 	vim.g.neovide_cursor_vfx_particle_density = 5.0
 end
 
+local dashboard_config = function()
+	vim.g.dashboard_footer_icon = "🐬 "
+	vim.g.dashboard_default_executive = "telescope"
+
+	vim.g.dashboard_custom_header = {
+		[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+		[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⣠⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+		[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣡⣾⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣟⠻⣿⣿⣿⣿⣿⣿⣿⣿]],
+		[[⣿⣿⣿⣿⣿⣿⣿⣿⡿⢫⣷⣿⣿⣿⣿⣿⣿⣿⣾⣯⣿⡿⢧⡚⢷⣌⣽⣿⣿⣿⣿⣿⣶⡌⣿⣿⣿⣿⣿⣿]],
+		[[⣿⣿⣿⣿⣿⣿⣿⣿⠇⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣇⣘⠿⢹⣿⣿⣿⣿⣿⣻⢿⣿⣿⣿⣿⣿]],
+		[[⣿⣿⣿⣿⣿⣿⣿⣿⠀⢸⣿⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⡟⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣻⣿⣿⣿⣿]],
+		[[⣿⣿⣿⣿⣿⣿⣿⡇⠀⣬⠏⣿⡇⢻⣿⣿⣿⣿⣿⣿⣿⣷⣼⣿⣿⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿]],
+		[[⣿⣿⣿⣿⣿⣿⣿⠀⠈⠁⠀⣿⡇⠘⡟⣿⣿⣿⣿⣿⣿⣿⣿⡏⠿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣇⣿⣿⣿⣿]],
+		[[⣿⣿⣿⣿⣿⣿⡏⠀⠀⠐⠀⢻⣇⠀⠀⠹⣿⣿⣿⣿⣿⣿⣩⡶⠼⠟⠻⠞⣿⡈⠻⣟⢻⣿⣿⣿⣿⣿⣿⣿]],
+		[[⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⢿⠀⡆⠀⠘⢿⢻⡿⣿⣧⣷⢣⣶⡃⢀⣾⡆⡋⣧⠙⢿⣿⣿⣟⣿⣿⣿⣿]],
+		[[⣿⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⡥⠂⡐⠀⠁⠑⣾⣿⣿⣾⣿⣿⣿⡿⣷⣷⣿⣧⣾⣿⣿⣿⣿⣿⣿⣿]],
+		[[⣿⣿⡿⣿⣍⡴⠆⠀⠀⠀⠀⠀⠀⠀⠀⣼⣄⣀⣷⡄⣙⢿⣿⣿⣿⣿⣯⣶⣿⣿⢟⣾⣿⣿⢡⣿⣿⣿⣿⣿]],
+		[[⣿⡏⣾⣿⣿⣿⣷⣦⠀⠀⠀⢀⡀⠀⠀⠠⣭⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⣡⣾⣿⣿⢏⣾⣿⣿⣿⣿⣿]],
+		[[⣿⣿⣿⣿⣿⣿⣿⣿⡴⠀⠀⠀⠀⠀⠠⠀⠰⣿⣿⣿⣷⣿⠿⠿⣿⣿⣭⡶⣫⠔⢻⢿⢇⣾⣿⣿⣿⣿⣿⣿]],
+		[[⣿⣿⣿⡿⢫⣽⠟⣋⠀⠀⠀⠀⣶⣦⠀⠀⠀⠈⠻⣿⣿⣿⣾⣿⣿⣿⣿⡿⣣⣿⣿⢸⣾⣿⣿⣿⣿⣿⣿⣿]],
+		[[⡿⠛⣹⣶⣶⣶⣾⣿⣷⣦⣤⣤⣀⣀⠀⠀⠀⠀⠀⠀⠉⠛⠻⢿⣿⡿⠫⠾⠿⠋⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+		[[⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣀⡆⣠⢀⣴⣏⡀⠀⠀⠀⠉⠀⠀⢀⣠⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+		[[⠿⠛⠛⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣯⣟⠷⢷⣿⡿⠋⠀⠀⠀⠀⣵⡀⢠⡿⠋⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+		[[⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⢿⣿⣿⠂⠀⠀⠀⠀⠀⢀⣽⣿⣿⣿⣿⣿⣿⣿⣍⠛⠿⣿⣿⣿⣿⣿⣿]],
+	}
+
+	vim.g.dashboard_custom_section = {
+		change_colorscheme = {
+			description = { " Scheme change              comma s c " },
+			command = "DashboardChangeColorscheme",
+		},
+		find_frecency = {
+			description = { " File frecency              comma f r " },
+			command = "Telescope frecency",
+		},
+		find_history = {
+			description = { " File history               comma f e " },
+			command = "DashboardFindHistory",
+		},
+		find_project = {
+			description = { " Project find               comma f p " },
+			command = "Telescope project",
+		},
+		find_file = {
+			description = { " File find                  comma f f " },
+			command = "DashboardFindFile",
+		},
+		file_new = {
+			description = { " File new                   comma f n " },
+			command = "DashboardNewFile",
+		},
+		find_word = {
+			description = { " Word find                  comma f w " },
+			command = "DashboardFindWord",
+		},
+	}
+end
+
 local load_core = function()
 	local pack = require("core.pack")
 
 	createdir()
-	-- disable_distribution_plugins()
+	disable_distribution_plugins()
 	leader_map()
 
-	pack.ensure_plugins()
+	pack:ensure_plugins()
 
 	neovide_config()
-	-- dashboard_config()
-	-- minimap_config()
-	-- clipboard_config()
+	dashboard_config()
 
 	require("core.options")
 	require("core.mapping")
-	--require("keymap")
+	require("keymap")
 	--require("core.event")
 
-	pack.load_compile()
+	pack:load_compile()
 
 	vim.cmd([[colorscheme catppuccin]])
 end
